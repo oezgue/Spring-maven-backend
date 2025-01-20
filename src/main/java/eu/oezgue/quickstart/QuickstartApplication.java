34d8c541e66cefe10bdfa59b8c9a278ea468a86c
Terlemez, Özgü <o.terlemez@reply.de>
2025-01-20 17:33:11 +0100

    Config Beans and Autowire them

--- src/main/java/eu/oezgue/quickstart/QuickstartApplication.java
+++ src/main/java/eu/oezgue/quickstart/QuickstartApplication.java
@@ -2,17 +2,27 @@ package eu.oezgue.quickstart;
 
 import eu.oezgue.quickstart.Example.Mensch;
 import eu.oezgue.quickstart.Example.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class QuickstartApplication {
+public class QuickstartApplication implements CommandLineRunner {
+	@Autowired
+	Mensch mensch;
 
 	public static void main(String[] args) {
 		SpringApplication.run(QuickstartApplication.class, args);
-		System.out.println("hello");
-		Mensch mensch = new Mensch(new Student("Patrick"));
-		mensch.sprich();
+		//System.out.println("hello");
+		//Mensch mensch = Mensch(new Student("Patrick"));
+		//this.mensch.sprich();
 	}
 
+
+	@Override
+	public void run(String... args) throws Exception {
+		//System.out.println("Hello Commander");
+		this.mensch.sprich();
+	}
 }
